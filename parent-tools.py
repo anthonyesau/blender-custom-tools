5e704646470f357dc0104969e078958b86b0e241
anthonyesau <anthony.esau@gmail.com>
2015-09-21 09:23:37 -0500

    Add neutralize transforms functionality

--- parent-tools.py
+++ parent-tools.py
@@ -29,6 +29,35 @@ if __name__ == "__main__":
     register()
 
 
+class NeutralizeTransformsOperator(bpy.types.Operator):
+    """Set the parent inverse matrix to current object transforms then clear all transforms. """
+    bl_idname = "object.neutralize_transforms"
+    bl_label = "Neutralize Transforms"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        for ob in context.selected_objects:
+            if ob.parent:
+                # Merge the inverse into the object's transforms
+                ob.matrix_parent_inverse = ob.matrix_parent_inverse * ob.matrix_basis
+                # Nullify the vestigial inverse matrix
+                ob.matrix_basis.identity()
+        return {'FINISHED'}
+
+def register():
+    bpy.utils.register_class(NeutralizeTransformsOperator)
+
+def unregister():
+    bpy.utils.unregister_class(NeutralizeTransformsOperator)
+
+if __name__ == "__main__":
+    register()
+
+
+
 # Create Parent Tools UI in the Tool Shelf of the 3D View
 class ParentToolsUI(bpy.types.Panel):
     """Better Parenting"""
@@ -40,8 +69,12 @@ class ParentToolsUI(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
+
         row = layout.row()
         row.operator("object.neutralize_parent_inverse")
+
+        row = layout.row()
+        row.operator("object.neutralize_transforms")
 
 
 def register():
