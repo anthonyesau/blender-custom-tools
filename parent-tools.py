a84cacc64ed3d0a828635b0497150f8a62ebbd4a
Anthony Esau <anthony.esau@gmail.com>
2015-09-21 23:04:41 -0500

    Add relative drivers functionality

--- parent-tools.py
+++ parent-tools.py
@@ -57,6 +57,47 @@ if __name__ == "__main__":
     register()
 
 
+class RelateDriversOperator(bpy.types.Operator):
+    """Set driver variable targets to relative objects (based on variable names containing 'self' and/or 'parent')"""
+    bl_idname = "object.relate_drivers"
+    bl_label = "Relate Drivers"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        for ob in bpy.context.selected_objects:
+            if ob.animation_data is not None and ob.animation_data.drivers is not None:
+                for driver in ob.animation_data.drivers:
+                    for variable in driver.driver.variables:
+                        name = variable.name
+                        print(type(variable.targets))
+                        for t in range(len(variable.targets)):
+                            print("name: " + name)
+                            print("iteration: " + str(t))
+                            print("index of 'self': " + str(name.find('self')))
+                            print("index of 'parent': " + str(name.find('parent')))
+                            self_index = name.find('self')
+                            parent_index = name.find('parent')
+                            print(self_index != -1)
+                            if (self_index < parent_index and self_index != -1) or (self_index > parent_index and parent_index == -1):
+                                    variable.targets[t].id = ob
+                                    name = name[self_index+4:len(name)]
+                            elif (parent_index < self_index and parent_index != - 1) or (parent_index > self_index and self_index == - 1):
+                                    variable.targets[t].id = ob.parent
+                                    name = name[parent_index+6:len(name)]
+        return {'FINISHED'}
+
+def register():
+    bpy.utils.register_class(RelateDriversOperator)
+
+def unregister():
+    bpy.utils.unregister_class(RelateDriversOperator)
+
+if __name__ == "__main__":
+    register()
+
 
 # Create Parent Tools UI in the Tool Shelf of the 3D View
 class ParentToolsUI(bpy.types.Panel):
@@ -75,6 +116,9 @@ class ParentToolsUI(bpy.types.Panel):
 
         row = layout.row()
         row.operator("object.neutralize_transforms")
+
+        row = layout.row()
+        row.operator("object.relate_drivers")
 
 
 def register():
