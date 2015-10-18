9c52778b1d2f9e3f9eab106e511c4144c7b360c9
anthonyesau <anthony.esau@gmail.com>
2015-10-17 22:29:37 -0500

    Integrate script from pink-vertex
    Solution to apply parent inverse matrix to animation keyframes was
    supplied by pink-vertex
    (http://blenderartists.org/forum/member.php?168207-pink-vertex)
    Original script:
    https://gist.github.com/pink-vertex/9167caf22c564acb0c92

--- parent-tools.py
+++ parent-tools.py
@@ -1,4 +1,125 @@
 import bpy
+import mathutils
+
+
+class Converter:
+    def __init__(self, ob, logging=False):
+        self.ob = ob
+        self.action = ob.animation_data.action
+        self.rot_mode = ob.rotation_mode
+
+        self.logging = logging
+        if self.logging:
+            print("\n"*2 + "-"*80)
+            print("Converting", ob.name)
+
+        # apply parent inverse to rest pose
+        self.parent_inv = ob.matrix_parent_inverse.copy()
+        if self.rot_mode == "AXIS_ANGLE":
+            mat = self.build_matrix(ob.location, ob.rotation_axis_angle, ob.scale)
+            mat = ob.matrix_parent_inverse * mat
+            loc, rot, scale = mat.decompose()
+
+            rot = rot.to_axis_angle()
+            rot = (rot[1], rot[0][0], rot[0][1], rot[0][2])
+
+            ob.location = loc
+            ob.rotation_axis_angle = rot
+            ob.scale = scale
+        else:
+            ob.matrix_basis = ob.matrix_parent_inverse * ob.matrix_basis
+        ob.matrix_parent_inverse.identity()
+
+        self.vec3_fs = "{: 8.4f} " * 3
+        self.vec4_fs = "{: 8.4f} " * 4
+
+        # gather fcurve channels
+        self.location = [None] * 3
+        self.rotation = [None] * (3 if self.rot_mode not in ("QUATERNION", "AXIS_ANGLE") else 4)
+        self.scale    = [None] * 3
+
+        self.frames = set()
+        prop_rot = ("rotation_quaternion" if self.rot_mode == "QUATERNION" else
+                    "rotation_axis_angle" if self.rot_mode == "AXIS_ANGLE" else
+                    "rotation_euler")
+
+        prop_to_attr = (
+            ("location", self.location),
+            (  prop_rot, self.rotation),
+            (   "scale", self.scale)
+        )
+
+        for fcu in self.action.fcurves:
+            for prop, attr in prop_to_attr:
+                if fcu.data_path == prop:
+                    attr[fcu.array_index] = fcu
+
+        for prop, attr in prop_to_attr:
+            for index, item in enumerate(attr):
+                # use constant float if no fcurve is present or fcurve is empty
+                if item is None or len(item.keyframe_points) == 0:
+                    attr[index] = getattr(ob, prop)[index]
+
+                # timeline
+                else:
+                    for kfp in item.keyframe_points:
+                        self.frames.add(kfp.co.x)
+
+    def eval_prop(self, prop, frame):
+        return tuple(elem.evaluate(frame) if isinstance(elem, bpy.types.FCurve) else
+                     elem for elem in prop)
+
+    def build_matrix(self, loc, rot, scale):
+        rot = (mathutils.Quaternion(rot)              if self.rot_mode == "QUATERNION" else
+               mathutils.Quaternion(rot[1:4], rot[0]) if self.rot_mode == "AXIS_ANGLE" else
+               mathutils.Euler(rot, self.rot_mode))
+
+        mat_loc = mathutils.Matrix.Translation(loc)
+        mat_rot = rot.to_matrix().to_4x4()
+        mat_sca = mathutils.Matrix.Identity(4)
+        for i in range(3):
+            mat_sca[i][i] = scale[i]
+
+        return mat_loc * mat_rot * mat_sca
+
+    def insert_keyframe(self, prop, frame, value):
+        for index, elem in enumerate(prop):
+            if isinstance(elem, bpy.types.FCurve):
+                elem.keyframe_points.insert(frame, value[index], {'REPLACE', 'FAST'})
+
+    def convert(self):
+        for frame in self.frames:
+            loc   = self.eval_prop(self.location, frame)
+            rot   = self.eval_prop(self.rotation, frame)
+            scale = self.eval_prop(self.scale,    frame)
+
+            if self.logging:
+                rot_fs = self.vec3_fs if self.rot_mode not in ("QUATERNION", "AXIS_ANGLE") else self.vec4_fs
+                print("frame %03d" % frame)
+                print("location before:" + self.vec3_fs.format(*loc))
+                print("rotation before:" +       rot_fs.format(*rot))
+                print("scale    before:" + self.vec3_fs.format(*scale))
+
+            mat_basis = self.build_matrix(loc, rot, scale)
+            mat_basis = self.parent_inv * mat_basis
+
+            loc, rot, scale = mat_basis.decompose()
+
+            if self.rot_mode == "AXIS_ANGLE":
+                rot = rot.to_axis_angle()
+                rot = (rot[1], rot[0][0], rot[0][1], rot[0][2])
+            elif not self.rot_mode == "QUATERNION":
+                rot = rot.to_euler(self.rot_mode)
+
+            if self.logging:
+                print("location after :" + self.vec3_fs.format(*loc))
+                print("rotation after :" +       rot_fs.format(*rot))
+                print("scale    after :" + self.vec3_fs.format(*scale))
+                print("-"*40)
+
+            self.insert_keyframe(self.location, frame, loc)
+            self.insert_keyframe(self.rotation, frame, rot)
+            self.insert_keyframe(self.scale,    frame, scale)
 
 
 class NeutralizeParentInverseOperator(bpy.types.Operator):
@@ -11,64 +132,15 @@ class NeutralizeParentInverseOperator(bpy.types.Operator):
         return context.active_object is not None
 
     def execute(self, context):
-
-        for ob in bpy.context.selected_objects: # Repeat for each selected object
-            if ob.parent:   # Only alter objects that have a parent
-
-                # Store the parent inverse matrix in separate location, rotation, and scale variables
-                parent_location_inverse, parent_rotation_inverse, parent_scale_inverse = ob.matrix_parent_inverse.decompose()
-
-                # Convert rotation quaternion to euler
-                parent_rotation_inverse = parent_rotation_inverse.to_euler()
-
-                # Apply the parent inverse matrix to the object's transforms
+        for ob in context.selected_objects:
+            if ob.parent and ob.animation_data:
+                converter = Converter(ob, logging=True)
+                converter.convert()
+            elif ob.parent:
+                # Merge the inverse into the object's transforms
                 ob.matrix_basis = ob.matrix_parent_inverse * ob.matrix_basis
-
                 # Clear the parent inverse matrix
                 ob.matrix_parent_inverse.identity()
-
-                print() # Blank lines for legibility
-                print()
-                print()
-                print()
-                print ("Location Inverse: " + str(list(parent_location_inverse)))
-                print ("Rotation Inverse: " + str(list(parent_rotation_inverse)))
-                print ("Scale Inverse: " + str(list(parent_scale_inverse)))
-
-                if ob.animation_data: # Check if object is animated
-                    action = ob.animation_data.action
-
-                    for fcu in action.fcurves: # Iterate through every f-curve
-
-                        print()
-                        print("Animated Property: " + fcu.data_path)
-                        print("Channel: " + str(fcu.array_index))
-                        # array_index of the f-curve is used as the index of the inverse that relates to the key frame
-                        # For example: if the data_path is 'location' and the array_index is 1
-                        # then the proper parent inverse to apply is parent_location_inverse[1]
-
-
-                        for i in range(len(fcu.keyframe_points)): # Iterate through all key frames
-
-                            print("Frame: " + str(fcu.keyframe_points[i].co.x))
-                            print("Original Value:" + str(fcu.keyframe_points[i].co.y))
-
-                            # Apply the parent inverse into the key frame values
-                            # Different operations for location, rotation, and scale
-                            if fcu.data_path == 'location':
-                                print("Specific Inverse: " + str(parent_location_inverse[fcu.array_index]))
-                                fcu.keyframe_points[i].co.y = fcu.keyframe_points[i].co.y + parent_location_inverse[fcu.array_index]
-
-                            elif fcu.data_path == 'rotation_euler':
-                                print("Specific Inverse: " + str(parent_rotation_inverse[fcu.array_index]))
-                                fcu.keyframe_points[i].co.y = parent_rotation_inverse[fcu.array_index] * fcu.keyframe_points[i].co.y
-
-                            elif fcu.data_path == 'scale':
-                                print("Specific Inverse: " + str(parent_scale_inverse[fcu.array_index]))
-                                fcu.keyframe_points[i].co.y = fcu.keyframe_points[i].co.y * parent_scale_inverse[fcu.array_index]
-
-                            print("New Value: " + str(fcu.keyframe_points[i].co.y))
-
         return {'FINISHED'}
 
 def register():
@@ -76,6 +148,76 @@ def register():
 
 def unregister():
     bpy.utils.unregister_class(NeutralizeParentInverseOperator)
+
+if __name__ == "__main__":
+    register()
+
+
+class NeutralizeTransformsOperator(bpy.types.Operator):
+    """Set the parent inverse matrix to current object transforms then clear all transforms"""
+    bl_idname = "object.neutralize_transforms"
+    bl_label = "Neutralize Transforms"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        for ob in context.selected_objects:
+            if ob.parent:
+                # Merge the inverse into the object's transforms
+                ob.matrix_parent_inverse = ob.matrix_parent_inverse * ob.matrix_basis
+                # Nullify the vestigial inverse matrix
+                ob.matrix_basis.identity()
+        return {'FINISHED'}
+
+def register():
+    bpy.utils.register_class(NeutralizeTransformsOperator)
+
+def unregister():
+    bpy.utils.unregister_class(NeutralizeTransformsOperator)
+
+if __name__ == "__main__":
+    register()
+
+
+class RelateDriversOperator(bpy.types.Operator):
+    """Set driver variable targets to relative objects (based on variable names containing 'self' and/or 'parent')"""
+    bl_idname = "object.relate_drivers"
+    bl_label = "Relate Drivers"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        for ob in bpy.context.selected_objects:
+            if ob.animation_data is not None and ob.animation_data.drivers is not None:
+                for driver in ob.animation_data.drivers:
+                    for variable in driver.driver.variables:
+                        name = variable.name
+                        print(type(variable.targets))
+                        for t in range(len(variable.targets)):
+                            print("name: " + name)
+                            print("iteration: " + str(t))
+                            print("index of 'self': " + str(name.find('self')))
+                            print("index of 'parent': " + str(name.find('parent')))
+                            self_index = name.find('self')
+                            parent_index = name.find('parent')
+                            print(self_index != -1)
+                            if (self_index < parent_index and self_index != -1) or (self_index > parent_index and parent_index == -1):
+                                    variable.targets[t].id = ob
+                                    name = name[self_index+4:len(name)]
+                            elif (parent_index < self_index and parent_index != - 1) or (parent_index > self_index and self_index == - 1):
+                                    variable.targets[t].id = ob.parent
+                                    name = name[parent_index+6:len(name)]
+        return {'FINISHED'}
+
+def register():
+    bpy.utils.register_class(RelateDriversOperator)
+
+def unregister():
+    bpy.utils.unregister_class(RelateDriversOperator)
 
 if __name__ == "__main__":
     register()
@@ -92,8 +234,16 @@ class ParentToolsUI(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
+
         row = layout.row()
         row.operator("object.neutralize_parent_inverse")
+
+        row = layout.row()
+        row.operator("object.neutralize_transforms")
+
+        row = layout.row()
+        row.operator("object.relate_drivers")
+
 
 def register():
     bpy.utils.register_class(ParentToolsUI)
